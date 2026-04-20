e9b0bbfbf798db77f128326af4055a64363e26e5
Antua <suleykin2005@mail.ru>
2026-04-20 08:43:37 +0300

    working prototype (-unused directive)

--- app/src/main/java/com/example/a51/MainActivity.kt
+++ app/src/main/java/com/example/a51/MainActivity.kt
@@ -4,36 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.a51.ui.theme.AddItemUseCase
 import com.example.a51.ui.theme.DataTxtSource
 import com.example.a51.ui.theme.DeleteItemUseCase
 import com.example.a51.ui.theme.GetAllUseCase
+import com.example.a51.ui.theme.Navigation
 import com.example.a51.ui.theme.RepositoryImpl
 import com.example.a51.ui.theme._51Theme
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.runtime.toString
-import androidx.navigation.NavHostController
-import com.example.a51.ui.theme.Navigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/a51/ReworkViewModel.kt
+++ app/src/main/java/com/example/a51/ReworkViewModel.kt
@@ -1,21 +1,14 @@
 package com.example.a51
 
-import android.app.Application
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.a51.ui.theme.AddItemUseCase
 import com.example.a51.ui.theme.DeleteItemUseCase
 import com.example.a51.ui.theme.GetAllUseCase
 import com.example.a51.ui.theme.RepositoryItem
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
